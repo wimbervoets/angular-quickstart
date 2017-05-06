cc371784e6a0b08bc0a5ded242b246e4bfe32abc
wbervoets <wim.bervoets@gmail.com>
2017-05-06 21:33:44 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,31 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { CurrencyComponent, CurrencyFormComponent } from "app/currencies.component";
 
+// import { AngularFireModule } from 'angularfire2';
+
+//https://github.com/angular/angularfire2/blob/master/docs/1-install-and-setup.md
+
+
+// // Must export the config
+// export const firebaseConfig = {
+//   apiKey: '<your-key>',
+//   authDomain: '<your-project-authdomain>',
+//   databaseURL: '<your-database-URL>',
+//   storageBucket: '<your-storage-bucket>',
+//   messagingSenderId: '<your-messaging-sender-id>'
+// };
+
 @NgModule({
   declarations: [
     AppComponent,
     CurrencyFormComponent,
-    CurrencyComponent,
+    CurrencyComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
+    // , AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
