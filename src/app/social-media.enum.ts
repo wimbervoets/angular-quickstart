ca3e41aba962737abb8eb97af998bd91d48f9f84
wbervoets <wim.bervoets@gmail.com>
2017-06-02 18:41:04 +0200

    update

---
+++ src/app/social-media.enum.ts
@@ -0,0 +1,26 @@
+export enum SocialMedia {
+
+    ADN,
+    BITBUCKET,
+    DROPBOX,
+    FACEBOOK,
+    FLICKR,
+    FOURSQUARE,
+    GITHUB,
+    GOOGLE,
+    INSTAGRAM,
+    LINKEDIN,
+    MICROSOFT,
+    ODNOKLASSNIKI,
+    OPENID,
+    PINTEREST,
+    REDDIT,
+    SOUNDCLOUD,
+    TUMBLR,
+    TWITTER,
+    VIMEO,
+    VK,
+    YAHOO
+}
+
+
